42b2241efbf8cbef3f6b0c2f9a55d423a23fe7ed
SoerenP <stivhobbykniv@gmail.com>
2023-07-28 12:46:45 +0200

    creating a jenkins container

--- cmd.sh
+++ cmd.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-
 set -e
 
 if [ "$ENV" = 'DEV' ]; then
     echo "Running development server"
     exec python "identidock.py"
+elif [ "$ENV" = 'UNIT' ]; then
+    echo "Running unit tests"
+    exec python "tests.py"
 else
     echo "Running production server"
     exec uwsgi --http 0.0.0.0:9090 --wsgi-file /app/identidock.py --callable app --stats 0.0.0.0:9191
